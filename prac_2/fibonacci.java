49d84d7d09093025df4c575ab3a11100d6ea224f
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-08-02 23:26:55 +0900

    Update fibonacci.java

--- prac_2/fibonacci.java
+++ prac_2/fibonacci.java
@@ -2,20 +2,20 @@ package prac_2;
 
 public class fibonacci {
     public static int fibonacci_iterative(int n) {
-        if (n < 1)
-            return n;
-        int temp = 0;
-        int first = 1;
-        for (int i = 2; i < n; i++) {
-            temp = first;
-            first += temp;
-            temp = first;
-        }
-        return temp;
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
 
+        int sum = 0;
+
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+            sum = arr[i];
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(fibonacci_iterative(2));
+        System.out.println(fibonacci_iterative(19));
     }
 }
